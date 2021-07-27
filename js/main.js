a2f3658d2d200684d7b5f52b577ed7d57cc2fafc
vlados096zver <81378975+vlados096zver@users.noreply.github.com>
2021-07-27 21:04:17 +0300

    update

---
+++ js/main.js
@@ -0,0 +1,163 @@
+ $(document).ready(function() {
+
+   $('.mobile-wrap').on('click', function() {
+     $(this).find('.main-nav__toggle').toggleClass('open');
+     $('.main-header__list').slideToggle();
+   });
+
+   $(window).resize(function() {
+
+     if ($(window).width() >= 780) {
+       $('.main-header__list').attr('style', '');
+       $('.main-nav__toggle').removeClass('open');
+     }
+
+   });
+
+    (function() {
+    var headerBlock = $('.main-header');
+
+    $(window).resize(listener);
+
+    $(window).scroll(listener)
+
+    function listener() {
+      var heightHeader;
+      var scrollTop = $(window).scrollTop();
+      var heightMenu = $('.main-header__menu').outerHeight(true);
+
+      heightHeader = $(".main-header__content").outerHeight(true);
+
+
+      if (heightHeader <= scrollTop && $(window).width() <= 1150) {
+       headerBlock.css({
+          'top': -heightHeader + heightMenu + 'px'
+        });
+      } else {
+        headerBlock.css({
+          'top': 0
+        });
+      }
+
+    }
+  })()
+
+   $('.main-header__list').on("click", ".main-header__link", function() {
+  var target = $(this).attr('href');
+  var value;
+  
+  if($(window).width() <= 1150) {
+    value = $(target).offset().top - $('.main-header__menu').outerHeight(true);
+  } else {
+    value =  $(target).offset().top - $('.main-header').outerHeight(true);
+  }
+  console.log($('.main-header').outerHeight(true))
+  $('html, body').animate({
+
+    scrollTop: value
+  }, 800);
+  return false;
+});
+
+
+   function validate(input, length, regExp, error, phone) {
+
+     $(input).on('blur keyup', function() {
+       var value = $(this).val();
+       var that = $(this);
+
+       regExp = regExp == '' ? /./ : regExp;
+
+       if (phone === true) {
+         bool_reg = !regExp.test(value);
+       } else {
+         bool_reg = regExp.test(value);
+       }
+
+       if (value.length > length && value !== '' && bool_reg) {
+         that.removeClass('form-fail').addClass('form-done');
+         $(error).slideUp();
+       } else {
+         that.removeClass('form-done').addClass('form-fail');
+         $(error).slideDown();
+       }
+     });
+
+   }
+
+   // деакцивация кнопки если есть поле с ошибкой
+
+   function disBtn(input, btn) {
+     var input = $(input);
+     input.on('blur keyup', function() {
+
+       if (input.hasClass('form-fail')) {
+         $(btn).attr('disabled', 'disabled');
+       } else {
+         $(btn).removeAttr('disabled');
+       }
+
+     });
+
+   }
+
+   // для проверки при нажатии
+
+   function valClick(input, length, regExp, error, btn, phone) {
+     var value = $(input).val();
+
+     regExp = regExp == '' ? /./ : regExp;
+
+     if (phone === true) {
+       bool_reg = regExp.test(value);
+     } else {
+       bool_reg = !regExp.test(value);
+     }
+
+     if (value.length < length && value === '' && bool_reg) {
+       $(input).addClass('form-fail');
+       $(error).slideDown();
+     }
+   }
+
+   //  деакцивация кнопки при нажатии
+
+   function disBtnClick(input, btn) {
+     var input = $(input);
+
+     if (input.hasClass('form-fail')) {
+       $(btn).attr('disabled', 'disabled');
+       return false;
+     } else {
+       return true;
+     }
+
+   }
+
+   function validateCheck(input) {
+     $(input).on('change', function() {
+
+       var check = $(this).prop('checked');
+       if (check) {
+         $('button').prop('disabled', false);
+       } else {
+         $('button').prop('disabled', true);
+       }
+     });
+   }
+
+   $('input[type="tel"]').mask("+38 (999) 999-99-99");
+
+   var regName = /^[a-zA-Zа-яА-ЯёЁ]+/;
+   var regEmail = /[-.\w]+@[-.\w]+\.[-.\w]+/i;
+   var regPhone = /[_]/i;
+   // пример использования
+
+   validate('#c_fio', 1, regName, '.contacts__fail-fio');
+   validate('#c_email', 1, regEmail, '.contacts__fail-email');
+   validate('#c_phone', 1, regPhone, '.contacts__fail-phone', true);
+
+   disBtn('#c_fio, #c_email, #c_phone', '.btn--send');
+
+
+ });
---
+++ js/utils.js
@@ -0,0 +1,6 @@
+'use strict';
+
+window.chartColors = {
+	color1: 'rgb(45, 190, 226)',
+	color2: 'rgb(173, 74, 244)'
+};
